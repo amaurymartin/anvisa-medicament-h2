f0103e1e3f348b65bb6e17b59f06c02f574af51a
Amaury Martin <amaury.conde@hotmail.com>
2021-05-25 23:21:04 -0300

    Init spring boot project

---
+++ src/main/java/br/com/amaurymartin/anvisamedicamenth2/AnvisaMedicamentH2Application.java
@@ -0,0 +1,13 @@
+package br.com.amaurymartin.anvisamedicamenth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnvisaMedicamentH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnvisaMedicamentH2Application.class, args);
+	}
+
+}
